b9ece91d0b4f61c2b025181aea5f8549e22128a3
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-05-23 23:29:41 -0300

    Export android app

---
+++ src/build-logic/src/main/kotlin/ext/ProjectExt.kt
@@ -0,0 +1,15 @@
+package ext
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.getByType
+
+private fun Project.libs() = project
+    .extensions
+    .getByType<VersionCatalogsExtension>()
+    .named("libs")
+
+fun Project.getVersionFromCatalogs(alias: String): String {
+    return libs()
+        .findVersion(alias).get().requiredVersion
+}
---
+++ src/build-logic/src/main/kotlin/plugins/android-library-plugin.gradle.kts
@@ -0,0 +1,32 @@
+import config.Config
+
+import org.gradle.kotlin.dsl.kotlin
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("kotlin-parcelize")
+}
+
+android {
+    compileSdk = Config.compileSdk
+
+    defaultConfig {
+        minSdk = Config.minSdk
+    }
+
+    compileOptions {
+        sourceCompatibility = Config.javaCompatibilityVersion
+        targetCompatibility = Config.javaCompatibilityVersion
+    }
+
+    testOptions {
+        unitTests.isReturnDefaultValues = Config.haveUnitTestsDefaultValues
+    }
+
+    kotlinOptions {
+        jvmTarget = Config.javaVMTarget
+    }
+}
