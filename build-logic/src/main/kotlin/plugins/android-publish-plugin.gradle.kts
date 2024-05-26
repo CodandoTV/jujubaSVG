713434fc934382b171978a8412c534359d011068
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-05-26 19:26:28 -0300

    New attempt

--- build-logic/src/main/kotlin/plugins/android-publish-plugin.gradle.kts
+++ build-logic/src/main/kotlin/plugins/android-publish-plugin.gradle.kts
@@ -3,19 +3,22 @@ plugins {
     id("com.android.library")
     id("maven-publish")
 }
-publishing {
-    repositories {
-        maven {
-            name = "JitPack"
-            url = uri("https://jitpack.io")
-        }
-    }
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "com.github.gabrielbmoro"
-            artifactId = "jujubaSVG"
 
-            from(components["release"])
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                name = "JitPack"
+                url = uri("https://jitpack.io")
+            }
+        }
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.github.gabrielbmoro"
+                artifactId = "jujubaSVG"
+
+                from(components["release"])
+            }
         }
     }
 }
