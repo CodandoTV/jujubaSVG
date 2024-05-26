acdf245e4dcd513c43ba5e0c41dbf4b16dd26843
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-05-25 22:25:45 -0300

    Add publishing block

--- jujubasvg/build.gradle.kts
+++ jujubasvg/build.gradle.kts
@@ -1,10 +1,28 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     id("android-compose-library-plugin")
+    id("maven-publish")
 }
 
 android {
     namespace = "com.gabrielbmoro.jujubasvg"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "com.github.gabrielbmoro"
+            artifactId = "jujubaSVG"
+            version = "0.0.3"
+        }
+    }
 }
 
 dependencies {
