9e3d975ed7dbcbe2cad7ecb1d816ab583950cd59
Arthur Henrique Henz <arthur.henz@edu.pucrs.br>
2024-07-15 01:59:21 -0300

    update tests

--- jujubasvg/src/test/kotlin/com/gabrielbmoro/jujubasvg/core/commander/JujubaCommanderTest.kt
+++ jujubasvg/src/test/kotlin/com/gabrielbmoro/jujubasvg/core/commander/JujubaCommanderTest.kt
@@ -1,5 +1,6 @@
 package com.gabrielbmoro.jujubasvg.core.commander
 
+import androidx.compose.ui.graphics.Color
 import app.cash.turbine.test
 import com.gabrielbmoro.jujubasvg.model.NodeCoordinate
 import kotlinx.coroutines.test.runTest
@@ -11,11 +12,11 @@ class JujubaCommanderTest {
     @Test
     fun `given an update background color command when it is invoked then emit the right jsCommand`() =
         runTest {
-            val jsCommand = "updateBackgroundColor('12','#0000');"
+            val jsCommand = "updateBackgroundColor('12','#0000ffff');"
             val commander = JujubaCommander()
 
             commander.command.test {
-                commander.execute(Command.UpdateBackgroundColor("12", "#0000"))
+                commander.execute(Command.UpdateBackgroundColor("12", Color.Blue))
 
                 val result = awaitItem()
                 assertEquals(jsCommand, result)
@@ -25,11 +26,11 @@ class JujubaCommanderTest {
     @Test
     fun `given an update stroke color command when it is invoked then emit the right jsCommand`() =
         runTest {
-            val jsCommand = "updateStrokeColor('12','#0000');"
+            val jsCommand = "updateStrokeColor('12','#ff0000ff');"
             val commander = JujubaCommander()
 
             commander.command.test {
-                commander.execute(Command.UpdateStrokeColor("12", "#0000"))
+                commander.execute(Command.UpdateStrokeColor("12", Color.Red))
 
                 val result = awaitItem()
                 assertEquals(jsCommand, result)
@@ -67,11 +68,11 @@ class JujubaCommanderTest {
     @Test
     fun `given a change root background color command when it is invoked then emit the right jsCommand`() =
         runTest {
-            val jsCommand = "updateRootBackgroundColor('#000000');"
+            val jsCommand = "updateRootBackgroundColor('#00ff00ff');"
             val commander = JujubaCommander()
 
             commander.command.test {
-                commander.execute(Command.UpdateRootBackgroundColor("#000000"))
+                commander.execute(Command.UpdateRootBackgroundColor(Color.Green))
 
                 val result = awaitItem()
                 assertEquals(jsCommand, result)
@@ -108,13 +109,13 @@ class JujubaCommanderTest {
     @Test
     fun `given the commands update background color and remove node when they are invoked then emit the right jsCommand`() =
         runTest {
-            val jsCommand = "updateBackgroundColor('12','#0000');\nremoveNode('12');"
+            val jsCommand = "updateBackgroundColor('12','#ffffffff');\nremoveNode('12');"
 
             val commander = JujubaCommander()
 
             commander.command.test {
                 commander.execute(
-                    Command.UpdateBackgroundColor("12", "#0000"),
+                    Command.UpdateBackgroundColor("12", Color.White),
                     Command.RemoveNode("12")
                 )
 
