439f4b300fd3fc984618502f6380fc72c12c595e
gabrielmoro <gabrielbronzattimoro.es@gmail.com>
2026-07-01 22:16:59 -0300

    feat: suppress FunctionNaming warning in MainViewController

--- kotlin/sampleApp/src/iosMain/kotlin/com/github/codandotv/jujubasvg/sampleapp/MainViewController.kt
+++ kotlin/sampleApp/src/iosMain/kotlin/com/github/codandotv/jujubasvg/sampleapp/MainViewController.kt
@@ -2,4 +2,5 @@ package com.github.codandotv.jujubasvg.sampleapp
 
 import androidx.compose.ui.window.ComposeUIViewController
 
+@Suppress("FunctionNaming")
 public fun MainViewController() = ComposeUIViewController { RootApp() }
