44b3e28c6eb79eb95d5dfedfcd931353ced9798d
gabrielmoro <gabrielbronzattimoro.es@gmail.com>
2026-08-03 09:47:15 -0300

    Update jvm toolchain

--- flutter/sample/android/settings.gradle.kts
+++ flutter/sample/android/settings.gradle.kts
@@ -20,6 +20,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
 include(":app")
